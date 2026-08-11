48e798a843099a82741ba4037983066e3eebd63c
emathcs <100460931+emathcs@users.noreply.github.com>
2026-08-11 17:22:18 -0600

    [MWT-10] Create the box structure (#8)
    Requirements:
    
    Create the box structure.
    
    Changes:
    
    - index.php: updating the div structures.
    - assets/css/minimalist.css: updating the styles.
    
    Co-authored-by: Carlos E. Alvarez <emathcs@gmail.com>

--- index.php
+++ index.php
@@ -8,83 +8,79 @@
         <?php wp_head(); ?>
     </head>
     <body>
-        <div id="page" class="site container">
-            <header id="masthead" class="site-header row">
-                <nav id="site-navigation" class="main-navigation navbar navbar-expand-lg fixed-top navbar-scroll">
-                    <div class="container">
-                        <div class="site-branding">
-                            <?php echo the_custom_logo(); ?>
-                            <p class="site-title">
-                                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="h1"><?php bloginfo( 'name' ); ?></a>
-                            </p>
-                        </div><!-- .site-branding -->
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                            <?php
-                            if ( has_nav_menu( 'primary' ) ) {
-                                wp_nav_menu(
-                                    array(
-                                        'theme_location'  => 'primary',
-                                        'menu_id'         => 'primary-menu',
-                                        'container_id'    => 'navbarNav',
-                                        'container_class' => 'navbar-collapse collapse justify-content-end',
-                                        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                                        'walker'          => new Custom_Walker_Nav_Menu(),
-                                        'container'       => 'ul',
-                                        'menu_class'      => 'navbar-nav'
-                                    )
-                                );
-                            }
-                            ?>
+        <div id="page" class="site">
+            <div id="page-container" class="site-container container">
+                <div id="page-container-row" class="site-container-row row">
+
+                    <header id="masthead" class="site-header">
+                        <div class="col-12 col-sm-12 site-header-container">
+                            <nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-scroll">
+                                <div class="site-branding">
+                                    <?php echo the_custom_logo(); ?>
+                                    <p class="site-title">
+                                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="h1"><?php bloginfo( 'name' ); ?></a>
+                                    </p>
+                                    <?php  if ( get_bloginfo( 'description', 'display' ) || is_customize_preview() ) : ?>
+                                        <p class="site-description"><?php echo get_bloginfo( 'description', 'display' ); ?></p>
+                                    <?php endif; ?>
+                                </div><!-- .site-branding -->
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                                    <?php
+                                    if ( has_nav_menu( 'primary' ) ) {
+                                        wp_nav_menu(
+                                            array(
+                                                'theme_location'  => 'primary',
+                                                'menu_id'         => 'primary-menu',
+                                                'container_id'    => 'navbarNav',
+                                                'container_class' => 'navbar-collapse collapse justify-content-end',
+                                                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                                                'walker'          => new Custom_Walker_Nav_Menu(),
+                                                'container'       => 'ul',
+                                                'menu_class'      => 'navbar-nav'
+                                            )
+                                        );
+                                    }
+                                    ?>
+                                </div>
+                            </nav><!-- #site-navigation -->
+                        </div>
+                    </header>
+                    <div id="content" class="site-content">
+                        <div id="primary" class="content-area">
+                            <main id="main" class="site-main">
+                                <?php if ( have_posts() ) : ?>
+                                    <?php while ( have_posts() ) : ?>
+                                        <?php the_post(); ?>
+                                        <h3><?php the_title(); ?></h3>
+                                        <?php the_content(); ?>
+                                        <?php wp_link_pages(); ?>
+                                        <?php edit_post_link(); ?>
+                                    <?php endwhile; ?>
+                                    <?php
+                                    if ( get_next_posts_link() ) {
+                                        next_posts_link();
+                                    }
+                                    ?>
+                                    <?php
+                                    if ( get_previous_posts_link() ) {
+                                        previous_posts_link();
+                                    }
+                                    ?>
+                                <?php else: ?>
+                                    <p>No posts found. :(</p>
+                                <?php endif; ?>
+                            </main>
                         </div>
                     </div>
-                </nav><!-- #site-navigation -->
-            </header>
-            <div id="content" class="site-content row">
-                <div id="primary" class="content-area">
-                    <main id="main" class="site-main">
-                        <?php if ( have_posts() ) : ?>
-                            <?php while ( have_posts() ) : ?>
-                                <?php the_post(); ?>
-                                <h3><?php the_title(); ?></h3>
-                                <?php the_content(); ?>
-                                <?php wp_link_pages(); ?>
-                                <?php edit_post_link(); ?>
-                            <?php endwhile; ?>
-                            <?php
-                            if ( get_next_posts_link() ) {
-                                next_posts_link();
-                            }
-                            ?>
-                            <?php
-                            if ( get_previous_posts_link() ) {
-                                previous_posts_link();
-                            }
-                            ?>
-                        <?php else: ?>
-                            <p>No posts found. :(</p>
-                        <?php endif; ?>
-                    </main>
+                    <footer id="colophon" class="site-footer">
+                        <?php wp_footer(); ?>
+                    </footer>
+
                 </div>
             </div>
-            <footer id="colophon" class="site-footer row">
-                <?php wp_footer(); ?>
-            </footer>
-            <script>
-                document.addEventListener("DOMContentLoaded", function(){
-                    window.addEventListener('scroll', function() {
-                        if (window.scrollY > 10) {
-                            document.getElementById('site-navigation').classList.add('navbar-scrolled');
-                            document.getElementById('site-navigation').classList.remove('navbar-dark');
-                        } else {
-                            document.getElementById('site-navigation').classList.remove('navbar-scrolled');
-                            document.getElementById('site-navigation').classList.remove('navbar-light');
-                        }
-                    });
-                });
-            </script>
         </div>
     </body>
 </html>
