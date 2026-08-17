ca41c465c48f4fff24704b70b2f824ec7964a217
Carlos E. Alvarez <emathcs@gmail.com>
2026-08-16 19:31:30 -0600

    Adding the entry meta section and updating styles

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,20 @@
         <?php else : ?>
             <?php the_title( '<h2 class="site-content-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
         <?php endif;?>
+        <div class="entry-meta">
+            <span class="author vcard">
+                <span>Created by</span>
+                <a class="url fn n" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) );  ?>"><?php echo esc_html( get_the_author() ); ?></a>,
+            </span>
+            <span class="posted-on">
+                <span>created on</span>
+                <time datetime="<?php echo get_the_date('Y-m-d'); ?>" itemprop="datePublished"><?php echo get_the_date( 'Y-m-d' ); ?></time>,
+            </span>
+            <span class="edited-on">
+                <span>edited on</span>
+                <time datetime="<?php echo get_the_modified_date('Y-m-d'); ?>" itemprop="datePublished"><?php echo get_the_modified_date( 'Y-m-d' ); ?></time>
+            </span>
+        </div><!-- .entry-meta -->
     </header>
     <div class="entry-content">
         <?php the_content(); ?>
