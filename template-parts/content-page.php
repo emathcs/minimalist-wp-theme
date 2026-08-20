2fdf406ee5ee486c18025d30b446bee5c7dd3f54
Carlos E. Alvarez <emathcs@gmail.com>
2026-08-19 19:59:42 -0600

    Hiding the site content title section in the front page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,9 +12,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php the_title('<h1 class="site-content-title">', '</h1>'); ?>
-    </header>
+    <?php if ( ! is_front_page() ) : ?>
+        <header class="entry-header">
+            <?php the_title('<h1 class="site-content-title">', '</h1>'); ?>
+        </header>
+    <?php endif; ?>
     <div class="entry-content">
         <?php the_content(); ?>
     </div>
