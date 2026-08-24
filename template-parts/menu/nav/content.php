22efe44176e864e2f24c69279c07da374a4cbe3a
emathcs <100460931+emathcs@users.noreply.github.com>
2026-08-24 16:52:46 -0600

    [MWT-30] Create the full width layout variant (#26)
    Requirements:
    
    Add the second layout variant, this variant will use the full width.
    
    Changes:
    
    Adding files to manage the full width in pages and posts
    
    - assets/css/minimalist.css: adding new styles for the full-width
    layout.
    - footer-full-width.php: new footer that uses full width page.
    - header-full-width.php: new header that uses full width page.
    - index.php: adding conditions to select the template.
    - page-full-width.php: new page layout that uses full width page.
    - single-full-width.php: new single layout that uses full width page.
    - template-parts/menu/menu-full-width.php: new menu that uses full width
    page.
    - template-parts/menu/nav/content.php: this file contains the logo,
    title, description and links.
    
    Co-authored-by: Carlos E. Alvarez <emathcs@gmail.com>

---
+++ template-parts/menu/nav/content.php
@@ -0,0 +1,46 @@
+<?php
+
+$menu = isset ( $args['menu'] ) ? $args['menu'] : null ;
+$menu = '' === $menu ? 'none' : $menu ;
+
+$description = isset ( $args['description'] ) ? $args['description'] : true ;
+
+$classes = array(
+    'none'      => '',
+    'primary'   => 'justify-content-end collapse',
+    'secondary' => 'justify-content-center',
+);
+
+?>
+
+<?php if ( 'primary' === $menu ) : ?>
+    <div class="site-branding">
+        <?php echo the_custom_logo(); ?>
+        <?php $h1p = is_front_page() ? 'h1' : 'p'; ?>
+        <?php echo '<' . $h1p . ' class="site-title"><a href="' . esc_url( home_url( '/' ) ) . '" rel="home">' . get_bloginfo( 'name' ) . '</a></' . $h1p . '>'; ?>
+        <?php  if ( $description && ( get_bloginfo( 'description', 'display' ) || is_customize_preview() ) ) : ?>
+            <p class="site-description"><?php echo get_bloginfo( 'description', 'display' ); ?></p>
+        <?php endif; ?>
+    </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav-<?php echo $menu ?>" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+<?php endif; ?>
+<div class="navbar-links navbar-collapse <?php echo $classes[$menu]; ?> navbarNav-<?php echo $menu; ?>" id="navbarNav-<?php echo $menu; ?>">
+    <?php
+    if ( has_nav_menu( $menu ) ) {
+        wp_nav_menu(
+            array(
+                'theme_location'  => $menu,
+                'menu_id'         => 'menu-' . $menu,
+                'container_id'    => 'navbarNav-' . $menu,
+                'container_class' => 'navbarNav-' . $menu,
+                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'walker'          => new Custom_Walker_Nav_Menu(),
+                'container'       => 'ul',
+                'menu_class'      => 'navbar-nav'
+            )
+        );
+    }
+    ?>
+</div>
